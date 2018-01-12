066040043a3f6a84dd9d37d856038594b6ac9bdd
1100177 <1100177@sktx1100177mn01>
2018-01-13 00:17:18 +0900

    init commit

---
+++ src/test/java/com/ccz/onurway/OnyourwayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ccz.onurway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnyourwayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
